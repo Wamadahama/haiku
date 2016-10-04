9991f9b302fc46d801b71e85097c006c2350c431
Elijah Ellis <smurftown2@gmail.com>
2016-10-04 14:18:47 -0400

    Test

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import json
 from bs4 import BeautifulSoup
 
 def main():
-    with open(Syllables.txt) as syll_data:
-        pass    
+    with open("Syllables.txt", encoding="cp1252") as syll_data:
+        raw_file_text = syll_data.readline()
+        print(raw_file_text.split("n·"))
+
 
 if __name__ == '__main__':
     main()
